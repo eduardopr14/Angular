5c6da51893e8a798581838d8929cf3f31099d231
Eduardo <eduardo.ruli@solinftec.com>
2021-01-12 10:45:45 -0300

    angular 01

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
+  photos = [
+    {
+      url: 'https://curiosamente.diariodepernambuco.com.br/wp-content/uploads/2017/07/le%C3%A3o.jpg',
+      description: 'Leão 1'
+    },
+    {
+      url: 'https://static5.depositphotos.com/1006472/535/i/600/depositphotos_5357980-stock-photo-white-lion.jpg',
+      description: 'Leão 2'
+    }
+  ]
+
 }
