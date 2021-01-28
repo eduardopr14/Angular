b80b8688d825d9f06d47888c5f9256c3a7ec8f17
eduardo.ruli@solinftec.com <eduardo.ruli@solinftec.com>
2021-01-28 15:12:05 -0300

    get content

--- angular-01/ReadingWebPage.java
+++ angular-01/ReadingWebPage.java
@@ -1,46 +1,61 @@
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Scanner;
 public class ReadingWebPage {
    public static void main(String args[]) throws IOException {
 
-      //Instantiating the URL class
-      URL url = new URL("https://www.basketball-reference.com/players/c/curryst01.html");
+      System.out.println(Arrays.toString(getStats("https://www.basketball-reference.com/players/d/doncilu01.html")));
 
-      //Retrieving the contents of the specified page
+   }
+
+   public static String getContent(String urlPlayer) throws IOException {
+
+      URL url = new URL(urlPlayer);
       Scanner sc = new Scanner(url.openStream());
-
-      //Instantiating the StringBuffer class to hold the result
       StringBuffer sb = new StringBuffer();
 
-      while(sc.hasNext()) {
+      while (sc.hasNext()) {
          sb.append(sc.next());
-         //System.out.println(sc.next());
       }
 
-      //Retrieving the String from the String Buffer object
       String result = sb.toString();
-      //System.out.println(result);
-      
-      //Removing the HTML tags
       result = result.replaceAll("<[^>]*>", "");
-      
-      int indexContent = result.indexOf("PTS");
-      //System.out.println("Index: " +indexContent);
-      String sub = result.substring(indexContent, indexContent + 7);
-      System.out.println(sub);
 
-      indexContent = result.indexOf("TRB");
-      //System.out.println("Index: " +indexContent);
-      sub = result.substring(indexContent, indexContent + 6);
-      System.out.println(sub);
+      // Get the Stats
+      int indexStats = result.indexOf("2020-21");
+      String subContent = result.substring(indexStats, indexStats + 100);
 
-      indexContent = result.indexOf("AST");
-      //System.out.println("Index: " +indexContent);
-      sub = result.substring(indexContent, indexContent + 6);
-      System.out.println(sub);
+      return subContent;
 
-      //System.out.println("Contents of the web page: " +result);
+   }
+
+   public static String[] getStats(String urlPlayer) throws IOException {
+
+      String subContent = getContent(urlPlayer);
+
+      String pts = getStat(subContent, "PTS", "TRB", 0);
+      String trb = getStat(subContent, "TRB", "AST", 31);
+      String ast = getStat(subContent, "AST", "FG%", 40);
+      String fg2 = getStat(subContent, "FG%", "FG3%", 50);
+      String fg3 = getStat(subContent, "FG3%", "FT%", 62);
+      String ft1 = getStat(subContent, "FT%", "eFG%", 72);
+
+      String[] stats = {pts, trb, ast, fg2, fg3, ft1};
+      return stats;
+
+   }
+
+   public static String getStat(String subContent, String statBeg, String statEnd, int value) {
+
+      int space = (statBeg == "FG3%") ? 1 : 0;
+
+      int beg = subContent.indexOf(statBeg);
+      int end = subContent.indexOf(".", value);
+
+      String sub = subContent.substring(beg + 3 + space, end + 2);
+
+      return statBeg + ": " + sub;
 
    }
 }
