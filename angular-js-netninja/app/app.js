8cbed776376e3a41c6a47c82adcd03d8958c1049
eduardo.ruli@solinftec.com <eduardo.ruli@solinftec.com>
2021-02-03 09:26:53 -0300

    url's

--- angular-js-netninja/app/app.js
+++ angular-js-netninja/app/app.js
@@ -1,6 +1,9 @@
 var myNinjaApp = angular.module('myNinjaApp', ['ngRoute', 'ngAnimate']);
 
-myNinjaApp.config(['$routeProvider', function($routeProvider){
+myNinjaApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    
+    $locationProvider.html5Mode(true);
+    
     $routeProvider
         .when('/home', {
             templateUrl: 'view/home.html',
